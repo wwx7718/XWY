807d8bc32188a27322aa038d8be326b3877676b2
wwx7718 <wwx7718@163.com>
2025-07-21 10:28:59 +0800

    screenpreview

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,19 +17,15 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-<<<<<<< HEAD
-}
 
-rootProject.name = "UI practice"
-include(":app")
- 
-=======
+//rootProject.name = "UI practice"
+//include(":app")
 
     versionCatalogs{
         create("libs")
     }
 }
 
+
 rootProject.name = "Application"
 include(":app")
->>>>>>> 54e6154 (screenpreview)
